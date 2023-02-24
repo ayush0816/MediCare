35c98bca4ce9baa7d2c844e85dd3e2e8af20c8df
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-02-25 03:23:58 +0530

    Json tokens

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const user = require("../models/user.js");
 const router = express.Router();
 const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/createUser", async (req, res) => {
   const { category, name, email, password, phone_no, ventilator_cnt } =
@@ -20,7 +21,8 @@ router.post("/createUser", async (req, res) => {
     ventilator_cnt: ventilator_cnt,
   });
   await user.save();
-  res.send(user);
+  const token = jwt.sign({ id: user._id }, "ayushsingh");
+  res.send(token);
 });
 
 router.post("/login", async (req, res) => {
@@ -30,7 +32,7 @@ router.post("/login", async (req, res) => {
 
   const cmp = bcrypt.compare(password, user.password);
   if (!cmp) return res.status(400).send({ err: "invalid email or pass" });
-
-  res.send(user);
+  const token = jwt.sign({ id: user._id }, "ayushsingh");
+  res.send(token);
 });
 module.exports = router;
