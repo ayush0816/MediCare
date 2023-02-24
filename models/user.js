c0955dd113f4a30ef7c2fb1ee1884a8d172a4051
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-02-25 02:20:02 +0530

    user models

--- models/user.js
+++ models/user.js
@@ -1,3 +1,34 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.model("User", {});
+module.exports = mongoose.model("User", {
+  category: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  phone_no: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  ventilator_cnt: {
+    type: Number,
+    default: 0,
+  },
+});
