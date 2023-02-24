21fc3acea1fb5b6f24a3df1c67d1cef567bc04a9
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-02-25 03:07:03 +0530

    adding secure pass

--- models/user.js
+++ models/user.js
@@ -25,7 +25,6 @@ module.exports = mongoose.model("User", {
   password: {
     type: String,
     required: true,
-    trim: true,
   },
   ventilator_cnt: {
     type: Number,
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,23 @@ const express = require("express");
 const user = require("../models/user.js");
 const router = express.Router();
 const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
 router.post("/createUser", async (req, res) => {
-  const user = new User(req.body);
+  const { category, name, email, password, phone_no, ventilator_cnt } =
+    req.body;
+
+  const check = await User.findOne({ email: email });
+  if (check) return res.status(400).send({ err: "Already registerd mail" });
+  const pass = await bcrypt.hash(password, 10);
+  const user = new User({
+    category: category,
+    name: name,
+    email: email,
+    phone_no: phone_no,
+    password: pass,
+    ventilator_cnt: ventilator_cnt,
+  });
   await user.save();
   res.send(user);
 });
@@ -14,8 +28,8 @@ router.post("/login", async (req, res) => {
   const user = await User.findOne({ email: email });
   if (!user) return res.status(400).send({ err: "invalid email or pass" });
 
-  if (user.password !== password)
-    return res.status(400).send({ err: "invalid email or pass" });
+  const cmp = bcrypt.compare(password, user.password);
+  if (!cmp) return res.status(400).send({ err: "invalid email or pass" });
 
   res.send(user);
 });
